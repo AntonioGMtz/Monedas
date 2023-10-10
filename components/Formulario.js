c5d1114a678c787cf5c10ecfcf4471d33969b7ef
Antonio <antoniogallegos086@gmail.com>
2023-10-10 15:47:03 -0600

    Adding datas to picker

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Text , View , StyleSheet} from 'react-native';
 import {Picker} from '@react-native-picker/picker';;
 
 
+const [moneda, guardarMoneda] = useState('');
+const [criptomoneda, guardarCriptomoneda] = useState('');
+
+const obtenerMoneda = moneda =>{
+    guardarMoneda(moneda)
+}
 
 const Formulario = () => {
     return(
         <View>
             <Text style={styles.label}>Monedad</Text>
 
-            <Picker>
+            <Picker
+                selectedValue={moneda}
+                onValueChange={moneda => obtenerMoneda(moneda)}
+            >
                 <Picker.Item label="- Seleccione -" value="" />
                 <Picker.Item label="Dolar de Estados Unidos" value="USD" />
                 <Picker.Item label="Peso Mexicano" value="MXN" />
